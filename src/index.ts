7d8cc38641447a4fc21562f33788a9a220702d46
Nicolaj Egelund <nicolaj@uptime.dk>
2020-07-17 18:34:37 +0200

    Add Sentry

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,12 @@ import { CommandoClient } from 'discord.js-commando'
 import { config } from 'dotenv'
 import { MusicQuiz } from './music-quiz'
 import path from 'path'
+import * as Sentry from '@sentry/node'
+import { RewriteFrames } from '@sentry/integrations'
 
 config({ path: path.resolve(__dirname, '../.env') })
+
+global.__rootdir__ = __dirname || process.cwd()
 
 declare global {
   namespace NodeJS {
@@ -17,8 +21,20 @@ declare global {
       SPOTIFY_CLIENT_ID: string
       SPOTIFY_CLIENT_SECRET: string
     }
+    interface Global {
+      __rootdir__: string
+    }
   }
 }
+
+Sentry.init({
+  dsn: 'https://63997f9fe0fa441ab00f6ff3926a2bb6@o100813.ingest.sentry.io/5342359',
+  integrations: [
+    new RewriteFrames({
+      root: global.__rootdir__
+    })
+  ]
+})
 
 Structures.extend('Guild', Guild => {
   class MusicGuild extends Guild {
